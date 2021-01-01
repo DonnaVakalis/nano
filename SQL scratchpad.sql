b9da37883e2a160b50027f6fc736959cd5222fe5
DonnaVakalis <donna.vakalis@utoronto.ca>
2021-01-01 13:00:44 -0500

    Module 3 continued

--- SQL scratchpad.sql
+++ SQL scratchpad.sql
@@ -114,3 +114,68 @@ LIMIT 1;
 	
 
 Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+
+
+SELECT COUNT(id), channel
+FROM web_events
+GROUP BY channel;
+	
+
+Who was the primary contact associated with the earliest web_event?
+
+
+SELECT a.primary_poc
+FROM web_events AS w
+JOIN accounts AS a
+ON w.account_id = a.id
+ORDER BY w.occurred_at
+LIMIT 1;
+	
+
+What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+
+
+SELECT MIN(o.total_amt_usd) AS min_order ,a.name
+FROM accounts AS a
+JOIN orders AS o
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY min_order;
+	
+
+For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type.
+
+
+SELECT a.name, AVG(o.standard_amt_usd) AS avg_std ,AVG(o.poster_amt_usd) AS avg_pstr
+FROM orders AS o
+JOIN accounts AS a
+ON a.id = o.account_id
+GROUP BY a.name;
+	
+
+Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+
+
+SELECT s.name AS rep, w.channel, COUNT(w) AS occurances
+FROM web_events AS w
+JOIN accounts AS a
+ON a.id = w.account_id
+JOIN sales_reps AS s
+ON s.id = a.sales_rep_id
+GROUP BY w.channel, rep
+ORDER BY occurances DESC;
+	
+
+Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+
+
+SELECT r.name, w.channel, COUNT (w.id) AS num_events
+FROM web_events AS w
+JOIN accounts AS a
+ON a.id = w.account_id
+JOIN sales_reps AS s
+ON a.sales_rep_id = s.id
+JOIN region AS r
+ON r.id = s.region_id
+GROUP BY r.name, w.channel
+ORDER BY num_events DESC;
