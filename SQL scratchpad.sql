65b84cc8df7e4d6ed4616f9dcc4a08fa88f89e8d
DonnaVakalis <donna.vakalis@utoronto.ca>
2021-01-17 11:39:01 -0500

    Finished Module 3

--- SQL scratchpad.sql
+++ SQL scratchpad.sql
@@ -341,3 +341,21 @@ ON o.account_id = a.id
 WHERE DATE_PART('year',o.occurred_at) = 2016  OR DATE_PART('year',o.occurred_at) = 2017
 GROUP BY 1
 ORDER BY lifetime_value DESC
+	
+
+The previous didn't account for the middle, nor the dollar amount associated with the sales. Management decides they want to see these characteristics represented as well. We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales. The middle group has any rep with more than 150 orders or 500000 in sales. Create a table with the sales rep name, the total number of orders, total sales across all orders, and a column with top, middle, or low depending on this criteria. Place the top sales people based on dollar amount of sales first in your final table. You might see a few upset sales people by this criteria!
+
+
+SELECT sr.name AS rep_name, 
+SUM(o.total_amt_usd) AS total_sales, 
+COUNT(o.id) AS num_orders, 
+CASE WHEN COUNT(o.id) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top' 
+WHEN COUNT(o.id) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle' 
+ELSE 'low' END AS rank
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+JOIN sales_reps sr
+ON sr.id = a.sales_rep_id
+GROUP BY 1
+ORDER BY 4 DESC, 3 DESC
